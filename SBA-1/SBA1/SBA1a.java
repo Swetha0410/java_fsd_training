fa12391b1f8e533b8cc5fb9df67849bdc79b19f9
Swetha0410 <99462377+Swetha0410@users.noreply.github.com>
2022-03-01 13:31:56 +0530

    Add files via upload

---
+++ SBA-1/SBA1/SBA1a.java
@@ -0,0 +1,16 @@
+//1.Create an array of 10 elements and print them using for each loop
+package SBA1;
+
+public class SBA1a {
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		for(int number: numbers) {
+			System.out.print(number);
+			System.out.print(" ");
+			
+		}
+	
+
+}
+}
